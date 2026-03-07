bb4c011d50d1068e740ae922c6afe495fb1b7a4e
JayadeepKondaveeti <jayadeepkondaveeti@gmail.com>
2026-03-07 16:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
 
     translated = ""
 
     if request.method == "POST":
-
         text = request.form["text"]
         target = request.form["language"]
 
@@ -21,10 +21,12 @@ def home():
         }
 
         response = requests.get(url, params=params)
+        data = response.json()
 
-        translated = response.json()['responseData']['translatedText']
+        translated = data["responseData"]["translatedText"]
 
     return render_template("index.html", translated=translated)
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
