9cd2bdf0f709560fd57b69e6aa76cff9ec2d51d9
Jayadeep Kondaveeti <jayadeepkondaveeti@gmail.com>
2026-03-13 19:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
+    return render_template("index.html")
 
-    translated = ""
+@app.route("/translate", methods=["POST"])
+def translate():
 
-    if request.method == "POST":
+    data = request.get_json()
 
-        text = request.form["text"]
-        target_language = request.form["language"]
+    text = data["text"]
+    language = data["language"]
 
-        url = "https://api.mymemory.translated.net/get"
+    url = "https://api.mymemory.translated.net/get"
 
-        params = {
-            "q": text,
-            "langpair": f"en|{target_language}"
-        }
+    params = {
+        "q": text,
+        "langpair": f"en|{language}"
+    }
 
-        try:
-            response = requests.get(url, params=params)
-            data = response.json()
-            translated = data["responseData"]["translatedText"]
-        except:
-            translated = "Translation failed. Please try again."
+    response = requests.get(url, params=params)
 
-    return render_template("index.html", translated=translated)
+    result = response.json()
+
+    translated = result["responseData"]["translatedText"]
+
+    return jsonify({"translated": translated})
 
 
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 5000))
+
     app.run(host="0.0.0.0", port=port)
